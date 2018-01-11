cd29091e07b6873b1776c1ec245cee38afa52046
Fisher Ankney <fisherankney@gmail.com>
2018-01-11 12:30:32 -0700

    complete chapter 14

---
+++ chapters/15_factors.R
@@ -0,0 +1,13 @@
+###########################
+##        Chapter 15     ##
+##         Factors       ## 
+###########################
+
+# Working through Hadley Wickham's R for Data Science 
+# Section 2, Chapter 15
+# Fisher Ankney 
+
+
+library('tidyverse')
+library('forcats')
+
