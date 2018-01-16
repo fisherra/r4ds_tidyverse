30b19a043ace98ffdc1c9db5d3a6e680eea43748
Fisher Ankney <fisherankney@gmail.com>
2018-01-16 11:12:31 -0700

    ch 21

---
+++ chapters/27_r_markdown.R
@@ -0,0 +1,11 @@
+############################
+##       Chapter 27       ##
+##       R Markdown       ## 
+############################
+
+# Working through Hadley Wickham's R for Data Science 
+# Section 5, Chapter 27
+# Fisher Ankney 
+
+# load necissary libraries
+library('tidyverse')
