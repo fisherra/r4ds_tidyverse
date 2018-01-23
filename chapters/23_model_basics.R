3b98e69f022dfc47d94a98a4e7283393da77ba0a
Fisher Ankney <fisherankney@gmail.com>
2018-01-23 14:36:35 -0700

    complete chapters, untidy

--- chapters/23_model_basics.R
+++ chapters/23_model_basics.R
@@ -44,7 +44,7 @@ models <- tibble(
   a2 = runif(250, -5, 5)
 ) 
 
-ggplot(sim1, aes(x, y)) + 
+gplot(sim1, aes(x, y)) + 
   geom_abline(aes(intercept = a1, slope = a2),
              data = models,
              alpha = 1/4) + 
@@ -121,8 +121,115 @@ ggplot(sim1, aes(x, y)) +
   geom_abline(intercept = best$par[1], slope = best$par[2])
 
 
+# fitting linear models 
+lm() 
+
+sim1_mod <- lm(y ~ x, data = sim1) 
+coef(sim1_mod)
+
+sim1a <- tibble(
+  x = rep(1:10, each = 3),
+  y = x * 1.5 + 6 + rt(length(x), df = 2)
+)
 
 
+# mean-absolute distance
+measure_distance <- function(mod, data) {
+  diff <- data$y - make_prediction(mod, data)
+  mean(abs(diff))
+}
+
+# understanding models and residuals 
+
+data_grid() # create even grid of values covering region where
+            # data lies
+
+grid <- sim1 %>% 
+  data_grid(x) 
+grid
+
+grid <- grid %>% 
+  add_predictions(sim1_mod)
+grid
+
+ ggplot(sim1, aes(x)) + 
+   geom_point(aes(y = y)) + 
+   geom_line(aes(y = pred), data = grid, color = "red", size = 1)
+
+# Residuals
+ # tells you what the model has missed, the distance between
+ # the predicted and observed value 
+
+add_residuals() 
+
+sim1 <- sim1 %>%
+  add_residuals(sim1_mod)
+sim1
+
+# understand the spread of the residual 
+ggplot(sim1, aes(resid)) +
+  geom_freqpoly(binwidth = 0.5)
+
+# average of residual will always be zero 
+# often want to recreate plots using the residual
+
+ggplot(sim1, aes(x, resid)) + 
+  geom_ref_line(h = 0) + 
+  geom_point()
+
+# when the graph looks like random noise the model fits
+# the data signal well 
+
+ggplot(sim2) + 
+  geom_point(aes(x,y))
+
+mod2 <- lm(y ~ x, data = sim2)
+
+grid <- sim2 %>%
+  data_grid(x) %>%
+  add_predictions(mod2)
+grid
+
+ggplot(sim2, aes(x)) + 
+  geom_point(aes(y = y)) + 
+  geom_point(data = grid, aes(y = pred), color = "red", size = 4)
+
+
+## Interactiosn (continous and categorical)
+
+ggplot(sim3, aes(x1, y)) + 
+  geom_point(aes(color = x2))
+
+mod1 <- lm(y ~ x1 + x2, data = sim3)
+mod2 <- lm(y ~ x1 * x2, data = sim3)
+
+grid <- sim3 %>%
+  data_grid(x1, x2) %>%
+  gather_predictions(mod1, mod2)
+grid
+
+ggplot(sim3, aes(x1, y, color = x2)) + 
+  geom_point() + 
+  geom_line(data = grid, aes(y = pred)) + 
+  facet_wrap(~ model)
+
+
+# facet models by x2
+
+sim3 <- sim3 %>% 
+  gather_residuals(mod1, mod2)
+
+ggplot(sim3, aes(x1, resid, colour = x2)) + 
+  geom_point() + 
+  facet_grid(model ~ x2)
+
+
+
+ 
+ 
+ 
+ 
+ 
 
 
 
